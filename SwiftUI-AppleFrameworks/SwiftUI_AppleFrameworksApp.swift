917d5f466d8e6caee00d15475abd89f80fc7ce55
Engin Bolat <engi.bolat@gmail.com>
2024-06-18 22:49:56 +0300

    Initial Commit

---
+++ SwiftUI-AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameworksApp.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by Engin Bolat on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
